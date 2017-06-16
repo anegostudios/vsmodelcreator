ce66444402a6d05a3d708e9a352c6b904ce12c69
Tyron Madlener <tyronx@gmail.com>
2017-06-16 10:31:11 +0200

    Added installer to build process for convenience

--- build.bat
+++ build.bat
@@ -9,4 +9,9 @@ mkdir windows
 xcopy ..\..\natives\windows windows /e
 cd ..
 cd ..
+
+..\..\binutil\ksignCMD.exe /f C:\Users\tyron\Dropbox\job\comodo_codesigning.pfx /p xLd2oGZ1S7CA+ output-bin/VSModelCreator.exe
+
+"..\binutil\InnoSetup5\ISCC.exe" innosetup.iss
+
 pause
