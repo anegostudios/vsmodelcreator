66c2f671d2c4a0c1272f9887ee8ec863397f1046
Tyron Madlener <tyronx@gmail.com>
2018-03-27 21:32:02 +0300

    Remove signing, remove some unnecessary files

--- build.bat
+++ build.bat
@@ -10,8 +10,6 @@ xcopy ..\..\natives\windows windows /e
 cd ..
 cd ..
 
-..\..\binutil\ksignCMD.exe /f C:\Users\tyron\Dropbox\job\comodo_codesigning.pfx /p xLd2oGZ1S7CA+ output-bin/VSModelCreator.exe
-
 "..\binutil\InnoSetup5\ISCC.exe" innosetup.iss
 
 pause
